4476be023a3fc02918998625c83230c1f239af0b
Afonso Gomes de Amorim <afonso.amorim2014@hotmail.com>
2022-05-20 20:53:36 -0300

    adicionando métodos

--- src/cursojava/executavel/PrimeiraClasseJava.java
+++ src/cursojava/executavel/PrimeiraClasseJava.java
@@ -10,12 +10,18 @@ public class PrimeiraClasseJava {
 		Aluno aluno1 = new Aluno();
 		aluno1.setNome("Afonso");
 		aluno1.setIdade(22);
+		aluno1.setNota1(10.0);
+		aluno1.setNota2(10.0);
+		aluno1.setNota3(10.0);
+		aluno1.setNota4(10.0);
 		
 		Aluno maria = new Aluno("Maria"); // construtor por padrão definido.
 		maria.setIdade(20);
 		
 		System.out.println(aluno1.getNome());
 		System.out.println(aluno1.getIdade());
+		System.out.println("A média de notas é "+aluno1.getMediaNota());
+		System.out.println("Resultado é " + (aluno1.getAlunoAprovado() ? "Aprovado" : "Reprovado"));
 		System.out.println(maria.getNome());
 		System.out.println(maria.getIdade());
 	}
