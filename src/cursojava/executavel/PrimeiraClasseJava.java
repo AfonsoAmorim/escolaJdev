3354edf610ad3f69683970c6f96140076c6eb813
Afonso Gomes de Amorim <afonso.amorim2014@hotmail.com>
2022-05-20 21:17:49 -0300

    adicionando interatividade no programa

--- src/cursojava/executavel/PrimeiraClasseJava.java
+++ src/cursojava/executavel/PrimeiraClasseJava.java
@@ -1,29 +1,36 @@
 package cursojava.executavel;
 
+import javax.swing.JOptionPane;
+
 import curso.java.jdev.classes.Aluno;
 
 public class PrimeiraClasseJava {
 
 	public static void main(String[] args) {
+		
+		String nome = JOptionPane.showInputDialog("Qual nome do Aluno ? ");
+		String idade = JOptionPane.showInputDialog("Qual idade do Aluno ? ");
+		String nota1 = JOptionPane.showInputDialog("Qual nota1 do Aluno ? ");
+		String nota2 = JOptionPane.showInputDialog("Qual nota2 do Aluno ? ");
+		String nota3 = JOptionPane.showInputDialog("Qual nota3 do Aluno ? ");
+		String nota4 = JOptionPane.showInputDialog("Qual nota4 do Aluno ? ");
 		// new Aluno() é uma instância (está criando o Objeto) 
 		// aluno1 e etc são referências para os objetos.
 		Aluno aluno1 = new Aluno();
-		aluno1.setNome("Afonso");
-		aluno1.setIdade(22);
-		aluno1.setNota1(10.0);
-		aluno1.setNota2(10.0);
-		aluno1.setNota3(10.0);
-		aluno1.setNota4(10.0);
-		
-		Aluno maria = new Aluno("Maria"); // construtor por padrão definido.
-		maria.setIdade(20);
+		aluno1.setNome(nome);
+		aluno1.setIdade(Integer.valueOf(idade));
+		aluno1.setNota1(Double.parseDouble(nota1));
+		aluno1.setNota2(Double.parseDouble(nota2));
+		aluno1.setNota3(Double.parseDouble(nota3));
+		aluno1.setNota4(Double.parseDouble(nota4));
+
 		
 		System.out.println(aluno1.getNome());
 		System.out.println(aluno1.getIdade());
 		System.out.println("A média de notas é "+aluno1.getMediaNota());
 		System.out.println("Resultado é " + (aluno1.getAlunoAprovado() ? "Aprovado" : "Reprovado"));
-		System.out.println(maria.getNome());
-		System.out.println(maria.getIdade());
+		
+		
 	}
 	
 }
